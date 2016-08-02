5d032ba512e632bf856f2a5e9f9623f813a76214
Eric dantarion Sheppard <dantarion@gmail.com>
2016-08-02 08:30:06 -0700

    More work on commandDB and parse output

--- bbscript_parser.py
+++ bbscript_parser.py
@@ -15,34 +15,50 @@ def parse_bbscript_routine(end = -1):
     global f,log
     currentCMD = -1
     currentIndicator = "_PRE"
+    currentFrame = 1
+    currentIndent = 0
     while currentCMD != 1 and f.tell() != end:
+
         loc = f.tell()
         currentCMD, = struct.unpack("<I",f.read(4))
+        if currentCMD in [4,14001]:
+            log.write("\n")
+        if currentCMD in [1,5,9,14002]:
+            currentIndent -= 1
+        indent = " "*4*currentIndent
         commandCounts[currentCMD] += 1
         if "format" not in commandDB[str(currentCMD)]:
-            if log: log.write("\tUnknown_{0}('{1}')\n".format(currentCMD,f.read(commandDB[str(currentCMD)]["size"]-4).encode("hex")))
+            s = indent+"Unknown_{0}('{1}')\n".format(currentCMD,f.read(commandDB[str(currentCMD)]["size"]-4).encode("hex"))
+            if log: log.write(s)
         else:
             dbData = commandDB[str(currentCMD)]
             if "name" not in dbData:
                 dbData["name"] = "Unknown{0}".format(currentCMD)
 
-            if "format" in dbData:
-                cmdData = list(struct.unpack(dbData["format"],f.read(struct.calcsize(dbData["format"]))))
+            cmdData = list(struct.unpack(dbData["format"],f.read(struct.calcsize(dbData["format"]))))
+
             if currentCMD == 0:
-                if log: log.write("@State(0x{0:X})\n".format(loc))
-                if log: log.write("def {0}():\n".format(cmdData[0].strip("\x00")))
+                if log: log.write(indent+"@State()\n".format(loc))
+                if log: log.write(indent+"def {0}():\n".format(cmdData[0].strip("\x00")))
                 currentIndicator = cmdData[0].strip("\x00")
             elif currentCMD == 8:
-                if log: log.write("\n@Subroutine(0x{0:X})\n".format(loc))
-                if log: log.write("def {0}():\n".format(cmdData[0].strip("\x00")))
+                if log: log.write(indent+"\n@Subroutine()\n".format(loc))
+                if log: log.write(indent+"def {0}():\n".format(cmdData[0].strip("\x00")))
                 currentIndicator = cmdData[0].strip("\x00")
             elif currentCMD in [1,9]:
                 pass
             else:
-                if log: log.write("\t{0}({1})\n".format(dbData["name"],",".join(map(sanitize,cmdData))))
-                commandCalls[currentCMD].append((currentIndicator,"\t{0}({1})\n".format(dbData["name"],",".join(map(sanitize,cmdData)))))
-        if currentCMD in [14002]:
+                if log: log.write(indent+"{0}({1})\n".format(dbData["name"],",".join(map(sanitize,cmdData))))
+                commandCalls[currentCMD].append((currentIndicator,"    {0}({1})\n".format(dbData["name"],",".join(map(sanitize,cmdData)))))
+        if currentCMD == 2:
+            if log:
+                log.seek(-2,1)
+                log.write("#Frame {0}->{1}\n".format(currentFrame,currentFrame+cmdData[1]))
+            currentFrame = currentFrame+cmdData[1]
+        if currentCMD in [5,14002]:
             log.write("\n")
+        if currentCMD in [0,4,8,14001]:
+            currentIndent += 1
 
 def parse_bbscript(filename, outfilename=None):
     global commandDB,f,log
@@ -65,9 +81,15 @@ def parse_bbscript(filename, outfilename=None):
         FUNCTION_OFFSET, = struct.unpack("<I",f.read(4))
         f.seek(4+0x24*FUNCTION_COUNT+FUNCTION_OFFSET)
         parse_bbscript_routine()
-parse_bbscript("output/char_rg_scr.pac.extracted/scr_rg.bin")
-print sorted(commandCounts.items(),cmp=lambda x,y: cmp(x[0],y[0]))
-for k,v in sorted(commandCalls.items(),cmp=lambda x,y: cmp(x[0],y[0])):
-    print k
-    for vv in v:
-        print v
+parse_bbscript("output/char_kk_scr.pac.extracted/scr_kk.bin","scr/kk_ex.py")
+parse_bbscript("output2/char_kk_scr.pac.extracted/scr_kk.bin","scr/kk_cp.py")
+total = sum(commandCounts.values())
+tableCount = 0
+for k,v in sorted(commandCounts.items(),cmp=lambda x,y: cmp(-x[1],-y[1])):
+
+    print "{4:3} {3:8.4f} {0:10} {1:10} {2}".format(k,v,commandDB[str(k)],float(v)/total*100,tableCount)
+    tableCount += 1
+#for k,v in sorted(commandCalls.items(),cmp=lambda x,y: cmp(x[0],y[0])):
+#    print k
+#    for vv in v:
+#        print v
